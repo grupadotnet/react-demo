898167834665d445727eaf7b39131572593c0cd4
arkadiusz-cholewa <arkadiusz.cholewaa@gmail.com>
2018-12-11 18:36:37 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Alert } from 'reactstrap';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     <div>Hello React!</div>
+    );
+  }
+}
+
+export default App;
