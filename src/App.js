c95669137fe6554545418433f25199bc5937d4b0
Arek Cholewa <arkadiusz.cholewaa@gmail.com>
2018-12-12 21:06:32 +0100

    step-1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: [],
+      hasData: false
+    };
+    console.log('constructor');
+  }
   render() {
+    console.log('render');
+    if (!this.state.hasData) return 'waiting...';
     return (
-     <div>Hello React!</div>
+      <div>Hello React!</div>
     );
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    this.setState(
+      {
+        hasData: true,
+        todos: [{ id: 1, name: 'foo', isCompleted: false }]
+      });
+    //console.log('todos', this.state.todos);
   }
 }
 
